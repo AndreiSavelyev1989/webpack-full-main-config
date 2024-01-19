1e4b7de1c018cb18e34659fccc6845bf673f1782
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-01-19 17:59:09 +0300

    added css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -5,13 +5,22 @@ import { BuildOptions } from "./types/types";
 export function buildLoader(options: BuildOptions): ModuleOptions["rules"] {
   const isProd = options.mode === "production";
 
+  const cssLoaderWithModules = {
+    loader: "css-loader",
+    options: {
+      modules: {
+        localIdentName: isProd ? "[hash:base64:8]" : "[path][name]__[local]",
+      },
+    },
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
       // Creates `style` nodes from JS strings
       isProd ? MiniCssExtractPlugin.loader : "style-loader",
       // Translates CSS into CommonJS
-      "css-loader",
+      cssLoaderWithModules,
       // Compiles Sass to CSS
       "sass-loader",
     ],
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
-import "./App.scss";
+import styles from "./App.module.scss";
 
 export const App = () => {
   const [count, setCount] = useState(0);
   return (
     <div>
-      <h1>{count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>Click</button>
+      <h1 className={styles.value}>{count}</h1>
+      <button
+        className={styles.button}
+        onClick={() => setCount((prev) => prev + 1)}
+      >
+        Click
+      </button>
     </div>
   );
 };
