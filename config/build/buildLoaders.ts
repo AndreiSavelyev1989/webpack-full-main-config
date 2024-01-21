6538940e862206742aec8d63159a0a3ec409e410
Andrei Savelyeu1 <andrei_savelyeu1@epam.com>
2024-01-21 14:10:15 +0300

    added assets loader/svgr loader

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -32,5 +32,33 @@ export function buildLoader(options: BuildOptions): ModuleOptions["rules"] {
     exclude: /node_modules/,
   };
 
-  return [scssLoader, tsLoader];
+  const assetsLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: "asset/resource",
+  };
+
+  const svgrLoader = {
+    test: /\.svg$/i,
+    issuer: /\.[jt]sx?$/,
+    use: [
+      {
+        loader: "@svgr/webpack",
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: "convertColors",
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  return [scssLoader, tsLoader, assetsLoader, svgrLoader];
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import styles from "./App.module.scss";
 import { Link, Outlet } from "react-router-dom";
+import imagePng from "@/assets/rocket.png";
+import imageJpg from "@/assets/tiger.jpg";
+import ImageSvg from "@/assets/freecodecamp.svg";
 
 export const App = () => {
   const [count, setCount] = useState(0);
@@ -10,6 +13,21 @@ export const App = () => {
       <br />
       <Link to={"/shop"}>Shop</Link>
       <br />
+
+      <div>
+        <label>Png image</label>
+        <img src={imagePng} alt="" width={"100px"} height={"100px"} />
+      </div>
+
+      <div>
+        <label>Jpg image</label>
+        <img src={imageJpg} alt="" width={"300px"} height={"200px"} />
+      </div>
+
+      <div>
+        <label>Svg image</label>
+        <ImageSvg className={styles.icon} width={"50px"} height={"50px"}/>
+      </div>
 
       <h1 className={styles.value}>{count}</h1>
       <button
